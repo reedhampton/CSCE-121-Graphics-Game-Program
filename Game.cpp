88c53423ae74f41659688debe15ff726e68a8123
reedhampton <reedhampton@tamu.edu>
2016-11-10 15:39:50 -0600

    Added Menu to GAME .Cpp
    Added menu and added back button functionalioty

--- Game.cpp
+++ Game.cpp
@@ -7,39 +7,49 @@ extern Window_Manager manager_instance;
 // - - - - - Constructor - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - //
 Game::Game(Point p , int w , int h , const string& name)		//Define our constructor
 : Graph_lib::Window(p, w, h, name),
-  back_button{Point {10, 10} , 50 , 30, "back" , cb_back}		//define the buttons we want to attach
+  back_button{Point {10, 10} , 50 , 30, "back" , cb_back} ,		//define the buttons we want to attach
+  menu_button{Point {400, 385} , 175 , 40, "Select the Difficulty Level" , cb_menu_pressed} ,
+  select_difficulty_menu{Point {455, 480} , 75 , 30, Menu::horizontal , "Select The Difficulty Level"} , 
+  difficulty_3_button(new Button {Point {455, 700} , 50 , 30, "3" , cb_select_difficulty})
 {
-	attach(back_button);											//Attach the button
+	attach(back_button);	//Attach the button
+	attach(menu_button);
+			select_difficulty_menu.attach(difficulty_3_button);
+	attach(select_difficulty_menu);
+	dynamic_cast<Game*>(manager_instance.game_window)->detach(select_difficulty_menu);
 }
 // - - - - - Back Button Callback and Function - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - //
-		void Game::cb_back(Address, Address pw)							//Define our call back function
+	void Game::cb_back(Address, Address pw)							//Define our call back function
 	{
 		reference_to<Game>(pw).back_button_function();
 	}
-	
-	
 	void Game::back_button_function()						//Define our call back function
 	{
+		dynamic_cast<Game*>(manager_instance.game_window)->menu_button.show();
+		dynamic_cast<Game*>(manager_instance.game_window)->detach(select_difficulty_menu);
 		manager_instance.game_window->hide();	
 		manager_instance.bootup_window->show();							// Run hide() on the member (bootup_window) of manager_instance
 	}
-// - - - - - Game Button Callback and Function - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - //	
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// - - - - - High Score Button Callback and Function - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - //
+	void Game::cb_menu_pressed(Address , Address pw)				//Define our callback function
+	{
+		reference_to<Game>(pw).menu_pressed();
+	}
+	void Game::menu_pressed()
+	{
+		dynamic_cast<Game*>(manager_instance.game_window)->menu_button.hide();
+		dynamic_cast<Game*>(manager_instance.game_window)->attach(select_difficulty_menu);
+	}
+// - - - - - Difficulty Level 3 Callback and Function - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - //	
+	void Game::cb_select_difficulty(Address , Address pw)				//Define our callback function
+	{
+		reference_to<Game>(pw).select_difficulty_button_function();
+	}
+	void Game::select_difficulty_button_function()
+	{
+		cout << "In difficulty selector";
+		
+	}
 // - - - - - High Score Button Callback and Function - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - //
 
 
--- Game.h
+++ Game.h
@@ -9,12 +9,24 @@ using namespace Graph_lib;
 class Game : public Graph_lib::Window	//Extend Bootup from Graph_lib::Window
 {
 	private:
+		int difficulty_level;						// 3 - 10
+	
 		Button back_button;											// Declare our button
+		Button menu_button;
+			Menu select_difficulty_menu;
+				Button* difficulty_3_button;
+		
 	public:
 		Game(Point p , int w , int h , const string& name);		//Declare our constructor
 		
 		static void cb_back(Address , Address);						//Declare our callback function
 		void back_button_function();
+		
+		static void cb_menu_pressed(Address , Address);		//Declare our callback function
+		void menu_pressed();
+		
+		static void cb_select_difficulty(Address , Address);		//Declare our callback function
+		void select_difficulty_button_function();
 };
 
 #endif
