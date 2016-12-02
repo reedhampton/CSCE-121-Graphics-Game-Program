2d1d79336d1be56c7d7a79659b4c92e31a9c3f41
samgiese9 <samgiese9@gmail.com>
2016-12-02 12:15:29 -0600

    Added difficulty buttons and out box
    Rearranged the select difficulty buttons, added an out box

--- Highscores.h
+++ Highscores.h
@@ -9,12 +9,44 @@ using namespace Graph_lib;
 class Highscores : public Graph_lib::Window	//Extend Tutorial from Graph_lib::Window
 {
 	private:
-		Button back_button;											// Declare our button
+		int difficulty_level;
+		
+		Text select_diff;
+		Text box_title;
+		
+		Button back_button;			// Declare our button
+		Button difficulty_3_highscores;
+		Button difficulty_4_highscores;
+		Button difficulty_5_highscores;
+		Button difficulty_6_highscores;
+		Button difficulty_7_highscores;
+				
+		//Out box for printing the scores		
+		Out_box show_scores;
+		
 	public:
 		Highscores(Point p , int w , int h , const string& name);		//Declare our constructor
 		
 		static void cb_back(Address , Address);						//Declare our callback function
 		void back_button_function();
+		
+		//static void cb_menu_pressed(Address , Address);		//Declare our callback function
+		//void hs_menu_pressed();
+		
+		static void cb_difficulty_3(Address , Address);		//Declare our callback function
+		void highscores_3_function();
+		
+		static void cb_difficulty_4(Address , Address);		//Declare our callback function
+		void highscores_4_function();
+		
+		static void cb_difficulty_5(Address , Address);		//Declare our callback function
+		void highscores_5_function();
+		
+		static void cb_difficulty_6(Address , Address);		//Declare our callback function
+		void highscores_6_function();
+		
+		static void cb_difficulty_7(Address , Address);		//Declare our callback function
+		void highscores_7_function();
 };
 
 #endif
